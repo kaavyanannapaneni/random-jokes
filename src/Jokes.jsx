54ee4a10d1addd40fd480a01ef30365d7050575a
Kaavya N <kaavyanannapaneni>
2025-12-03 18:50:32 +0530

    Error handling

--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -5,39 +5,51 @@ import './index.css'
 const Jokes = () => {
   const [show, setShow] = useState(false)
   const [type, setType] = useState('general')
-  const { data, refetch, isLoading, isFetching } = useQuery({
+  const { data, refetch, isLoading, isFetching, isError, error } = useQuery({
     queryKey: ['jokes', type],
     queryFn: () => getJokes(type),
   })
-
-  if (data) {
-    console.log(data)
-  }
 
   const handleJokeType = (type) => {
     setType(type)
     refetch()
     setShow(false)
   }
-  
+
   return (
     <>
+      <h1>
+        <span>J</span>
+        <span>O</span>
+        <span>K</span>
+        <span>E</span>
+        <span>S</span>
+      </h1>
       <div className='container'>
         {isLoading || isFetching ? (
           <div className='loader'></div>
         ) : (
           <>
-            <h1>{data[0]?.setup}</h1>
-            {show && <h1>{data[0]?.punchline}</h1>}
-            {!show && (
-              <button
-                onClick={() => {
-                  setShow(true)
-                }}
-              >
-                Punchline
-              </button>
+            {isError ? (
+              <>
+                <h2>{error.message}</h2>
+              </>
+            ) : (
+              <>
+                <h2>{data[0]?.setup}</h2>
+                {show && <h2>{data[0]?.punchline}</h2>}
+                {!show && (
+                  <button
+                    onClick={() => {
+                      setShow(true)
+                    }}
+                  >
+                    Punchline
+                  </button>
+                )}
+              </>
             )}
+
             {/* {show && (
               <button
                 onClick={() => {
@@ -82,10 +94,14 @@ const Jokes = () => {
 
 const getJokes = async (type) => {
   await new Promise((resolve) => setTimeout(resolve, 1000))
-  const response = await fetch(
-    `https://official-joke-api.appspot.com/jokes/${type}/random`
-  )
-  return await response.json()
+  try {
+    const response = await fetch(
+      `https://official-joke-api.appspot.com/jokes/${type}/random`
+    )
+    return await response.json()
+  } catch (error) {
+    throw new Error('Oops.. this page took a day off.')
+  }
 }
 
 export default Jokes
