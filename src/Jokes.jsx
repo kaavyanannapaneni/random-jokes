e1294d6e3659d3f3db7d7e92412e0262d61b6563
Kaavya N <kaavyanannapaneni>
2025-12-03 15:42:52 +0530

    Adding buttons to generate jokes

--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -4,48 +4,83 @@ import './index.css'
 
 const Jokes = () => {
   const [show, setShow] = useState(false)
+  const [type, setType] = useState('general')
   const { data, refetch, isLoading, isFetching } = useQuery({
-    queryKey: ['jokes'],
-    queryFn: getJokes,
+    queryKey: ['jokes', type],
+    queryFn: () => getJokes(type),
   })
 
+  if (data) {
+    console.log(data)
+  }
+
   return (
-    <div className='container'>
-      {isLoading || isFetching ? (
-        <div className='loader'></div>
-      ) : (
-        <>
-          <h1>{data?.setup}</h1>
-          {show && <h1>{data?.punchline}</h1>}
-          {!show && (
-            <button
-              onClick={() => {
-                setShow(true)
-              }}
-            >
-              Punchline
-            </button>
-          )}
-          {show && (
-            <button
-              onClick={() => {
-                setShow(false)
-                refetch()
-              }}
-            >
-              Next
-            </button>
-          )}
-        </>
-      )}
-    </div>
+    <>
+      <div className='container'>
+        {isLoading || isFetching ? (
+          <div className='loader'></div>
+        ) : (
+          <>
+            <h1>{data[0]?.setup}</h1>
+            {show && <h1>{data[0]?.punchline}</h1>}
+            {!show && (
+              <button
+                onClick={() => {
+                  setShow(true)
+                }}
+              >
+                Punchline
+              </button>
+            )}
+            {/* {show && (
+              <button
+                onClick={() => {
+                  refetch()
+                }}
+              >
+                Next
+              </button>
+            )} */}
+          </>
+        )}
+      </div>
+      <div className='joke-options'>
+        <button
+          onClick={() => {
+            setType('programming')
+            refetch()
+            setShow(false)
+          }}
+        >
+          Programming
+        </button>
+        <button
+          onClick={() => {
+            setType('general')
+            refetch()
+            setShow(false)
+          }}
+        >
+          General
+        </button>
+        <button
+          onClick={() => {
+            setType('knock-knock')
+            refetch()
+            setShow(false)
+          }}
+        >
+          Knock-Knock
+        </button>
+      </div>
+    </>
   )
 }
 
-const getJokes = async () => {
+const getJokes = async (type) => {
   await new Promise((resolve) => setTimeout(resolve, 1000))
   const response = await fetch(
-    `https://official-joke-api.appspot.com/jokes/random/`
+    `https://official-joke-api.appspot.com/jokes/${type}/random`
   )
   return await response.json()
 }
